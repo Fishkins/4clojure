e216447fd87d97ce78c6b4f67d95dc73254a05e4
fishkins <squall1386@hotmail.com>
2014-11-15 16:45:46 -0500

    Convert 127 to a single function

--- 127.clj
+++ 127.clj
@@ -1,63 +1,67 @@
 ;; Love Triangle
 
-(defn parse-board [rocks]
-    (let [to-bits
-        (fn to-bits [dec-num]
-          (loop [cur-num dec-num
-                 bits    ()]
-            (if (= 0 cur-num)
-              bits
-              (recur (int (/ cur-num 2)) (cons (rem cur-num 2) bits)))))
-
-        uneven-rocks
-        (map to-bits rocks)
-
-        max-len
-        (apply max (map count uneven-rocks))
-
-        pad-bin
-        (fn [bin]
-          (->
-           (- max-len (count bin))
-           (repeat 0)
-           (concat bin)))]
-
-      (vec (map (comp vec pad-bin) uneven-rocks))))
-
-(defn bottom-left-triangle-size [board-piece]
-  (let [len (count board-piece)
-        triangle-parts (for [x (range len)
-                             y (range len)
-                             :when (<= (+ x y) (dec len))]
-                         (get-in board-piece [x y]))]
-    (if (every? #(= 1 %) triangle-parts)
-      (count triangle-parts)
-      0)))
-
-(defn top-center-triangle-size [board-piece]
-  (let [height (count board-piece)
-        width  (count (board-piece 0))
-        subrow #(take (- width (* 2 %)) (drop % (board-piece %)))
-        triangle-parts (mapcat subrow (range height))]
-    
-    (if (every? #(= 1 %) triangle-parts)
-      (count triangle-parts)
-      0)))
-
-(defn all-sub-boards [board]
-  (let [height (count board)
-        width  (count (board 0))]
-    (for [
-          y-side (range 2 (inc height))
-          x-side (range 2 (inc width))
-          y-off  (range (inc (- height y-side)))
-          x-off  (range (inc (- width x-side)))
-          ]
-      (vec (map #(vec (take x-side (drop x-off %1)))
-                (take y-side (drop y-off board)))))))
-
-(defn biggest-triangle [rocks]
+(fn biggest-triangle [rocks]
   (let [
+        parse-board
+        (fn parse-board [rocks]
+          (let [to-bits
+                (fn to-bits [dec-num]
+                  (loop [cur-num dec-num
+                         bits    ()]
+                    (if (= 0 cur-num)
+                      bits
+                      (recur (int (/ cur-num 2)) (cons (rem cur-num 2) bits)))))
+
+                uneven-rocks
+                (map to-bits rocks)
+
+                max-len
+                (apply max (map count uneven-rocks))
+
+                pad-bin
+                (fn [bin]
+                  (->
+                   (- max-len (count bin))
+                   (repeat 0)
+                   (concat bin)))]
+
+            (vec (map (comp vec pad-bin) uneven-rocks))))
+
+        bottom-left-triangle-size
+        (fn bottom-left-triangle-size [board-piece]
+          (let [len (count board-piece)
+                triangle-parts (for [x (range len)
+                                     y (range len)
+                                     :when (<= (+ x y) (dec len))]
+                                 (get-in board-piece [x y]))]
+            (if (every? #(= 1 %) triangle-parts)
+              (count triangle-parts)
+              0)))
+
+        top-center-triangle-size
+        (fn top-center-triangle-size [board-piece]
+          (let [height (count board-piece)
+                width  (count (board-piece 0))
+                subrow #(take (- width (* 2 %)) (drop % (board-piece %)))
+                triangle-parts (mapcat subrow (range height))]
+            
+            (if (every? #(= 1 %) triangle-parts)
+              (count triangle-parts)
+              0)))
+
+        all-sub-boards
+        (fn all-sub-boards [board]
+          (let [height (count board)
+                width  (count (board 0))]
+            (for [
+                  y-side (range 2 (inc height))
+                  x-side (range 2 (inc width))
+                  y-off  (range (inc (- height y-side)))
+                  x-off  (range (inc (- width x-side)))
+                  ]
+              (vec (map #(vec (take x-side (drop x-off %1)))
+                        (take y-side (drop y-off board)))))))
+
         board (parse-board rocks)
 
         transpose
@@ -78,7 +82,7 @@
           (vec (rotation board)))
 
         pieces
-        (mapcat all-sub-boards orientations)
+        (set (mapcat all-sub-boards orientations))
 
         corner-sizes
         (for [piece pieces
@@ -104,17 +108,3 @@
       nil
       max-size))
   )
-
-;; Tests
-
-(defn __ [rocks]
-  (biggest-triangle rocks))
-
-;; (= 9 (__ [18 7 14 14 6 3]))
-;; (= 4 (__ [7 3]))
-;; (= nil (__ [21 10 21 10]))
-;; (= nil (__ [0 31 0 31 0]))
-;; (= 6 (__ [17 22 6 14 22]))
-;; (= 3 (__ [3 3]))
-;; (= 15 (__ [1 3 7 15 31]))
-;; (= 10 (__ [15 15 15 15 15]))
